89ce8e5a92b38e2aee4c759e7b6eb55c48c49f11
mitoma <mutetheradio@gmail.com>
2017-11-26 20:53:24 +0900

    パターンマッチをmapに変更

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,36 @@
 use std::cmp::Ordering;
-use std::net::TcpListener;
-use std::io::{BufRead, BufReader};
-
-fn start_server() {
-    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
-    for stream in listener.incoming() {
-        match stream {
-            Ok(stream) => {
-                let stream = stream;
-                let mut buf = String::new();
-                let mut stream = BufReader::new(stream);
-                'request: loop {
-                    let len = stream.read_line(&mut buf);
-                    match len {
-                        Ok(len) => match len.cmp(&0) {
-                            Ordering::Equal => {
-                                println!("len = 0");
-                                break 'request;
-                            }
-                            Ordering::Greater => println!("{}", buf),
-                            _ => {}
-                        },
-                        Err(_) => break 'request,
-                    }
-                }
-            }
-            Err(_) => {}
-        }
-    }
-}
+use std::net::{TcpListener, TcpStream};
+use std::io::{BufRead, BufReader, Write};
 
 fn main() {
     println!("Hello server.");
     start_server();
+}
+
+fn start_server() {
+    let listener = TcpListener::bind("127.0.0.1:8080").expect("port listen failed.");
+    for stream in listener.incoming() {
+        stream.map(echo);
+    }
+}
+
+fn echo(stream: TcpStream) {
+    let mut stream = stream.try_clone().expect("stream clone failed.");
+    let mut reader = BufReader::new(stream.try_clone().expect("stream clone failed."));
+    'request: loop {
+        let mut buf = String::new();
+        match reader.read_line(&mut buf) {
+            Ok(len) => match len.cmp(&0) {
+                Ordering::Equal => {
+                    println!("len = 0");
+                    break 'request;
+                }
+                Ordering::Greater => {
+                    let _ = stream.write(&buf.into_bytes());
+                }
+                _ => {}
+            },
+            Err(_) => break 'request,
+        }
+    }
 }
